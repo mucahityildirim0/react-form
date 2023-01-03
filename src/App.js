c7a182a8c5fd2bd048fae534e47884f793210a72
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2023-01-03 22:57:46 +0300

    add input validation and displaying errors

--- src/App.js
+++ src/App.js
@@ -17,20 +17,27 @@ function App() {
       name: "username",
       type: "text",
       placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
       label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
     },
     {
       id: 2,
       name: "email",
       type: "text",
       placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
       label: "Email",
+      required: true,
     },
     {
       id: 3,
       name: "birthday",
       type: "text",
       placeholder: "Birthday",
+      errorMessage: "",
       label: "Birthday",
     },
     {
@@ -38,14 +45,21 @@ function App() {
       name: "password",
       type: "password",
       placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
       label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
     },
     {
       id: 5,
       name: "confirmPassword",
       type: "password",
       placeholder: "Confirm Password",
+      errorMessage: "Passwords don't match!",
       label: "Confirm Password",
+      pattern: values.password,
+      required: true,
     },
   ];
 
